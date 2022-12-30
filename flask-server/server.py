efa479cdc4f2defaab83cb94548c5e99ce41f506
Antonio Netto <60482472+duzisnetto@users.noreply.github.com>
2022-12-30 11:10:50 -0300

    ampliando a api para receber/enviar arquivos de imagem

--- flask-server/server.py
+++ flask-server/server.py
@@ -3,8 +3,10 @@ from pymongo import MongoClient
 from flask_pymongo import ObjectId
 from flask_cors import CORS
 from produtos import *
-import json 
+import json
+import os
 from bson.json_util import dumps
+
 
 app = Flask(__name__)
 URI = "mongodb+srv://duzis:duzis@cluster0.fwzx7jb.mongodb.net/?retryWrites=true&w=majority"
@@ -12,44 +14,83 @@ mongo = MongoClient(URI)
 collectionProdutos = mongo.testando.produtos
 collectionUsers = mongo.testando.users
 
+# ARQUIVOS #######################
+
+
+@app.route('/arquivo', methods=['POST'])
+def mandaArquivo():
+    arquivo = request.files.get('imagem')
+    nome_do_arquivo = request.files.get('imagem').filename
+    print(nome_do_arquivo)
+
+    arquivo.save(os.path.join('./imagens/' + nome_do_arquivo))
+
+    return "cheguei aqui"
+
+
+@app.route('/arquivos', methods=['GET'])
+def lista_arquivos():
+    arquivos = []
+
+    for nome_do_arquivo in os.listdir('imagens'):
+        endereco_do_arquivo = os.path.join('./imagens/' + nome_do_arquivo)
+        if (os.path.isfile(endereco_do_arquivo)):
+            arquivos.append(nome_do_arquivo)
+    return jsonify(arquivos)
+
+
+@app.route("/arquivo/<nome_do_aquivo>" methods=["GET"])
+def get_arquivo(nome_do_arquivo):
+    return send_from_directory('./imagens/' + nome_do_arquivo)
+
+
 # PRODUTOS #######################
+
 
 @app.route('/produtos', methods=['POST'])
 def adicionar():
     collectionProdutos.insert_one(request.json)
+
     return "Adicionado"
+
 
 @app.route('/produtos/<id>', methods=['PUT'])
 def editar(id):
     produto = collectionProdutos.find_one({"_id": ObjectId(id)}, request.json)
     return "Editado"
 
+
 @app.route('/produtos/<id>', methods=['DELETE'])
 def deletar(id):
     collectionProdutos.delete_one({"_id": ObjectId(id)})
     return "Deletado"
 
+
 @app.route("/produtos", methods=["GET"])
 def visualizarTodos():
     return dumps(collectionProdutos.find())
 
-@app.route("/produto/<id>", methods=["GET"] )
+
+@app.route("/produto/<id>", methods=["GET"])
 def produto(id):
-    return dumps(collectionProdutos.find_one({"_id" : ObjectId(id)}))
-     
+    return dumps(collectionProdutos.find_one({"_id": ObjectId(id)}))
+
 # USUÁRIOS #######################
+
 
 @app.route('/user/<id>', methods=['GET'])
 def getUser(id):
-    return dumps(user = collectionUsers.find_one({
+    return dumps(user=collectionUsers.find_one({
         '_id': ObjectId(id)
     }))
-    
+
+
 @app.route('/user/<id>', methods=['PUT'])
 def putUser(id):
-    user = collectionUsers.update_one({'_id': ObjectId(id)},{
+    user = collectionUsers.update_one({'_id': ObjectId(id)}, {
         '$set': request.json
     })
+
 
 @app.route('/user/<id>', methods=['DELETE'])
 def deleteUser(id):
@@ -57,15 +98,18 @@ def deleteUser(id):
         '_id': ObjectId(id)
     })
     return "Deletado"
-    
+
+
 @app.route('/users', methods=['POST'])
 def createUser():
     collectionUsers.insert_one(request.json)
     return "received"
 
+
 @app.route('/users', methods=['GET'])
 def lista():
     return dumps(collectionUsers.find())
+
 
 if __name__ == '__main__':
     # DEBUG is SET to TRUE. CHANGE FOR PROD
